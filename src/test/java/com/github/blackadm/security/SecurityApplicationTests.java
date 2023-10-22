56078dba003076e0eeaf1362a38ca67c807ce6d2
black-adm <mattheuscontato17@gmail.com>
2023-10-22 11:06:46 -0300

    first commit

---
+++ src/test/java/com/github/blackadm/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.blackadm.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
