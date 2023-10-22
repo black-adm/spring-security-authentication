69390baca23d9f74edd00b0e4c087d2344902cba
black-adm <mattheuscontato17@gmail.com>
2023-10-22 11:51:23 -0300

    Updating pom.xml to test my product listing controller

--- src/main/java/com/github/blackadm/security/modules/product/controllers/ProductController.java
+++ src/main/java/com/github/blackadm/security/modules/product/controllers/ProductController.java
@@ -1,4 +1,23 @@
 package com.github.blackadm.security.modules.product.controllers;
 
+import com.github.blackadm.security.modules.product.entities.Product;
+import com.github.blackadm.security.modules.product.services.ListProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    ListProductService listProductService;
+
+    @GetMapping
+    public List<Product> list() {
+        return listProductService.listAll();
+    }
 }
